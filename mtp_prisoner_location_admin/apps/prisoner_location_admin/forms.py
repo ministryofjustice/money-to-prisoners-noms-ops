663a6cdcf8ad1b021b143dd4fdb45ee8ccf40b38
Ian Brechin <ian.brechin@gmail.com>
2015-09-16 11:34:42 +0100

    Propagate validation errors through to the user from the API
    If the uploaded file containts data which fails validation at the
    API, this should be reported to the user

--- mtp_prisoner_location_admin/apps/prisoner_location_admin/forms.py
+++ mtp_prisoner_location_admin/apps/prisoner_location_admin/forms.py
@@ -3,8 +3,11 @@ import io
 
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from slumber.exceptions import SlumberHttpBaseException
 
 from moj_auth import api_client
+
+EXPECTED_ROW_LENGTH = 3
 
 
 class LocationFileUploadForm(forms.Form):
@@ -19,9 +22,10 @@ class LocationFileUploadForm(forms.Form):
         location_reader = csv.reader(
             io.StringIO(self.cleaned_data['location_file'].read().decode('utf-8')))
         for row in location_reader:
-            if len(row) != 3:
+            if len(row) != EXPECTED_ROW_LENGTH:
                 raise forms.ValidationError(
-                    _("Location file contains an invalid row"))
+                    _("Row has %s values, should have %s: %s"
+                        % (len(row), EXPECTED_ROW_LENGTH, row)))
 
             locations.append({
                 'prisoner_number': row[0],
@@ -38,4 +42,8 @@ class LocationFileUploadForm(forms.Form):
     def update_locations(self):
         locations = self.cleaned_data['location_file']
         client = api_client.get_connection(self.request)
-        client.prisoner_locations.post(locations)
+
+        try:
+            client.prisoner_locations.post(locations)
+        except SlumberHttpBaseException as e:
+            raise forms.ValidationError(e.content)
--- mtp_prisoner_location_admin/apps/prisoner_location_admin/views.py
+++ mtp_prisoner_location_admin/apps/prisoner_location_admin/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse_lazy
+from django import forms
 
 from .forms import LocationFileUploadForm
 
@@ -15,5 +16,9 @@ class LocationFileUploadView(FormView):
         return kwargs
 
     def form_valid(self, form):
-        form.update_locations()
+        try:
+            form.update_locations()
+        except forms.ValidationError as e:
+            form.add_error(None, e)
+            return self.form_invalid(form)
         return super(LocationFileUploadView, self).form_valid(form)
