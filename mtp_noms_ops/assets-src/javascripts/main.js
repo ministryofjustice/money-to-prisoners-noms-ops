6de1b12fec005d8e2849fe281994a860cefb3087
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-09-22 10:34:34 +0100

    Include selection-buttons.js script to fix hit area selection

--- mtp_noms_ops/assets-src/javascripts/main.js
+++ mtp_noms_ops/assets-src/javascripts/main.js
@@ -6,6 +6,7 @@
   require('analytics').Analytics.init();
   require('help-popup').HelpPopup.init();
   require('collapsing-table').CollapsingTable.init();
+  require('selection-buttons').SelectionButtons.init();
 
   require('security-forms').SecurityForms.init();
 })();
