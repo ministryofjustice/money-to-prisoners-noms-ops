69faead3142299eb828d4dacefab8e2b123e3cf8
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-10-27 17:35:13 +0100

    Remove breadcrumbs from feedback pages (replaced with back button)

--- mtp_noms_ops/apps/feedback/urls.py
+++ mtp_noms_ops/apps/feedback/urls.py
@@ -1,16 +1,9 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
-from django.utils.translation import gettext_lazy as _
 from zendesk_tickets import views
 from zendesk_tickets.forms import EmailTicketForm
 
-extra_context = {
-    'breadcrumbs': [
-        {'name': _('Home'), 'url': '/'},
-        {'name': _('Contact us')},
-    ]
-}
 
 urlpatterns = [
     url(r'^$', views.ticket,
@@ -20,11 +13,9 @@ urlpatterns = [
             'success_redirect_url': reverse_lazy('feedback_success'),
             'subject': 'MTP NOMS Ops Feedback',
             'tags': ['feedback', 'mtp', 'noms-ops', settings.ENVIRONMENT],
-            'extra_context': extra_context,
         }, name='submit_ticket'),
     url(r'^success/$', views.success,
         {
             'template_name': 'mtp_common/feedback/success.html',
-            'extra_context': extra_context,
         }, name='feedback_success'),
 ]
