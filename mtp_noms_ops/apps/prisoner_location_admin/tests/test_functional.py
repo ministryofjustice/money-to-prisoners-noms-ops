863d0ff9271637421cced403bbc89cc608680893
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-11-15 13:00:50 +0000

    Fix functional test

--- mtp_noms_ops/apps/prisoner_location_admin/tests/test_functional.py
+++ mtp_noms_ops/apps/prisoner_location_admin/tests/test_functional.py
@@ -41,8 +41,7 @@ class LoginTests(PrisonerLocationAdminTestCase):
     def test_title(self):
         self.driver.get(self.live_server_url + '/en-gb/')
         heading = self.driver.find_element_by_tag_name('h1')
-        self.assertEqual('NOMS admin', heading.text)
-        self.assertEqual('48px', heading.value_of_css_property('font-size'))
+        self.assertEqual('NOMS admin\nSign in', heading.text)
 
     def test_bad_login(self):
         self.login('prisoner-location-admin', 'bad-password')
