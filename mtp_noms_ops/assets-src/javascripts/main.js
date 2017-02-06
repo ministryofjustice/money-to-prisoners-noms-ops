15110bfa561fec44171ae25da2a016487cf0717c
Ian Brechin <ian.brechin@gmail.com>
2017-02-06 16:07:59 +0000

    Ensure that credit checking JS works in IE8

--- mtp_noms_ops/assets-src/javascripts/main.js
+++ mtp_noms_ops/assets-src/javascripts/main.js
@@ -1,14 +1,14 @@
 (function () {
   'use strict';
-  require('security-forms').SecurityForms.init();
-  require('tabs').Tabs.init();
   require('confirm-checked').ConfirmChecked.init();
+  require('dialog').Dialog.init();
+  require('tabs').Tabs.init();
+  require('security-forms').SecurityForms.init();
 
-  require('proposition-header').PropositionHeader.init();
   require('upload').Upload.init();
-  require('analytics').Analytics.init();
+  require('proposition-header').PropositionHeader.init();
   require('help-popup').HelpPopup.init();
   require('print').Print.init();
+  require('analytics').Analytics.init();
   require('selection-buttons').SelectionButtons.init();
-  require('dialog').Dialog.init();
 }());
