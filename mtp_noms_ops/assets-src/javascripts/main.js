bb44fef0670aea5449017c1423f280f8a109a586
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-05-19 16:21:28 +0100

    Update class names used by GOV.UK styles

--- mtp_noms_ops/assets-src/javascripts/main.js
+++ mtp_noms_ops/assets-src/javascripts/main.js
@@ -1,15 +1,15 @@
 (function () {
   'use strict';
-  require('confirm-checked').ConfirmChecked.init();
-  require('dialog').Dialog.init();
-  require('tabs').Tabs.init();
-  require('security-forms').SecurityForms.init();
+  require('polyfills').Polyfills.init();
 
+  require('dialog').Dialog.init();
   require('upload').Upload.init();
   require('proposition-header').PropositionHeader.init();
   require('help-popup').HelpPopup.init();
   require('print').Print.init();
   require('analytics').Analytics.init();
-  require('polyfills').Polyfills.init();
-  require('selection-buttons').SelectionButtons.init();
+
+  require('confirm-checked').ConfirmChecked.init();
+  require('security-forms').SecurityForms.init();
+  require('tabs').Tabs.init();
 }());
