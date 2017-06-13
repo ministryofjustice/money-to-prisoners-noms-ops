b61036aa3ebdd3eed4ccfa2d1cae09726667d408
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-06-13 14:15:22 +0100

    Remove unused JS module

--- mtp_noms_ops/assets-src/javascripts/main.js
+++ mtp_noms_ops/assets-src/javascripts/main.js
@@ -5,7 +5,6 @@
   require('dialog').Dialog.init();
   require('upload').Upload.init();
   require('proposition-header').PropositionHeader.init();
-  require('help-popup').HelpPopup.init();
   require('print').Print.init();
   require('analytics').Analytics.init();
 
