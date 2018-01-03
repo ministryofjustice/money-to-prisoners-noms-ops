17b236a3f230407d302fe797a4ce70002132fd31
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-01-03 13:37:13 +0000

    Update get-help class-based views

--- mtp_noms_ops/apps/feedback/urls.py
+++ mtp_noms_ops/apps/feedback/urls.py
@@ -1,21 +1,16 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
-from zendesk_tickets import views
-from zendesk_tickets.forms import EmailTicketForm
+from mtp_common.views import GetHelpView as BaseGetHelpView, GetHelpSuccessView
+
+
+class GetHelpView(BaseGetHelpView):
+    success_url = reverse_lazy('feedback_success')
+    ticket_subject = 'MTP NOMS Ops Feedback'
+    ticket_tags = ['feedback', 'mtp', 'noms-ops', settings.ENVIRONMENT]
 
 
 urlpatterns = [
-    url(r'^$', views.ticket,
-        {
-            'form_class': EmailTicketForm,
-            'template_name': 'mtp_common/feedback/submit_feedback.html',
-            'success_redirect_url': reverse_lazy('feedback_success'),
-            'subject': 'MTP NOMS Ops Feedback',
-            'tags': ['feedback', 'mtp', 'noms-ops', settings.ENVIRONMENT],
-        }, name='submit_ticket'),
-    url(r'^success/$', views.success,
-        {
-            'template_name': 'mtp_common/feedback/success.html',
-        }, name='feedback_success'),
+    url(r'^$', GetHelpView.as_view(), name='submit_ticket'),
+    url(r'^success/$', GetHelpSuccessView.as_view(), name='feedback_success'),
 ]
