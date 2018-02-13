b1c0e69a47fec4b9621e10e329df468151e36f35
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-02-13 14:30:49 +0000

    Replace repetitive HTML labelling with a template tag

--- mtp_noms_ops/apps/security/templatetags/security.py
+++ mtp_noms_ops/apps/security/templatetags/security.py
@@ -2,7 +2,8 @@ import logging
 
 from django import template
 from django.core.urlresolvers import reverse
-from django.utils.html import format_html_join
+from django.utils.crypto import get_random_string
+from django.utils.html import format_html, format_html_join
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext
@@ -208,3 +209,15 @@ def find_rejection_reason(comment_set):
     for comment in filter(lambda comment: comment['category'] == 'reject', comment_set):
         return comment['comment']
     return ''
+
+
+@register.simple_tag
+def labelled_data(label, value, tag='div'):
+    element_id = get_random_string(length=4)
+    return format_html(
+        '''
+        <div id="mtp-label-{element_id}" class="mtp-detail-label">{label}</div>
+        <{tag} aria-labelledby="mtp-label-{element_id}">{value}</{tag}>
+        ''',
+        element_id=element_id, label=label, value=value, tag=tag
+    )
