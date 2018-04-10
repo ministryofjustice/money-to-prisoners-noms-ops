e4dac73e108de6e91dbf7bc1600a90d4c76eca36
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-04-10 12:42:57 +0100

    Add email domain name correction suggestion to email input fields

--- mtp_noms_ops/assets-src/javascripts/main.js
+++ mtp_noms_ops/assets-src/javascripts/main.js
@@ -8,6 +8,11 @@
   require('print').Print.init();
   require('analytics').Analytics.init();
   require('tabbed-panel').TabbedPanel.init();
+  require('mailcheck-warning').MailcheckWarning.init(
+    '.mtp-account-management input[type=email]',
+    ['hmps.gsi.gov.uk', 'noms.gsi.gov.uk', 'justice.gsi.gov.uk'],
+    ['gsi.gov.uk', 'gov.uk']
+  );
 
   require('confirm-checked').ConfirmChecked.init();
   require('security-forms').SecurityForms.init();
