772e9a0e3de12055248397555231f1b589c734bf
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-06-02 15:47:30 +0100

    Fix functional test since title had changed previously

--- mtp_noms_ops/apps/prisoner_location_admin/tests/test_functional.py
+++ mtp_noms_ops/apps/prisoner_location_admin/tests/test_functional.py
@@ -40,7 +40,7 @@ class LoginTests(PrisonerLocationAdminTestCase):
     def test_title(self):
         self.driver.get(self.live_server_url + '/en-gb/')
         heading = self.driver.find_element_by_tag_name('h1')
-        self.assertEqual('Prisoner money intelligence\nSign in', heading.text)
+        self.assertEqual('Monitor prisoner money', heading.text)
 
     def test_bad_login(self):
         self.login('prisoner-location-admin', 'bad-password')
