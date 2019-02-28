90b12f1ed3b91d5709a5031bd0087c6d8a7c8b0e
Ian Brechin <ian.brechin@gmail.com>
2019-02-28 15:33:45 +0000

    Remove error squashing so that user is correctly logged out
    Error squashing around the prison list retrieval meant that when
    a user's session expired they would get a 500 rather than being
    logged out correctly.

--- mtp_noms_ops/apps/security/forms/preferences.py
+++ mtp_noms_ops/apps/security/forms/preferences.py
@@ -6,7 +6,6 @@ from django.core.cache import cache
 from django.utils.translation import gettext_lazy as _
 from mtp_common.api import retrieve_all_pages_for_path
 from mtp_common.user_admin.forms import ApiForm
-from oauthlib.oauth2 import OAuth2Error
 from requests import RequestException
 
 from security.utils import refresh_user_data
@@ -17,12 +16,9 @@ logger = logging.getLogger('mtp')
 def prison_choices(api_session):
     choices = cache.get('prison-list')
     if not choices:
-        try:
-            choices = retrieve_all_pages_for_path(api_session, '/prisons/', exclude_empty_prisons=True)
-            choices = [[prison['nomis_id'], prison['name']] for prison in choices]
-            cache.set('prison-list', choices, timeout=60 * 60 * 6)
-        except (RequestException, OAuth2Error, ValueError):
-            logger.exception('Could not look up prison list')
+        choices = retrieve_all_pages_for_path(api_session, '/prisons/', exclude_empty_prisons=True)
+        choices = [[prison['nomis_id'], prison['name']] for prison in choices]
+        cache.set('prison-list', choices, timeout=60 * 60 * 6)
     return choices
 
 
