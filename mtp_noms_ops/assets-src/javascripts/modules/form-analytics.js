07a34edba7a1ae8bc0a3d6691b5093757ffb6039
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-03-15 14:23:03 +0000

    Re-enable print/export click GA event reporting with overwritten page/location/title and add to new credit check page

--- mtp_noms_ops/assets-src/javascripts/modules/form-analytics.js
+++ mtp_noms_ops/assets-src/javascripts/modules/form-analytics.js
@@ -3,41 +3,46 @@
 var analytics = require('analytics');
 
 exports.FormAnalytics = {
-  selector: '.js-FormAnalytics',
-
   init: function () {
-    this.cacheEls();
-    this.bindEvents();
+    $('.js-FormAnalytics').each(this.bindForm);
   },
 
-  cacheEls: function () {
-    this.$form = $(this.selector);
-  },
-
-  bindEvents: function () {
-    this.$form.on('submit', this.onSubmit);
-  },
-
-  onSubmit: function () {
+  bindForm: function () {
     var $form = $(this);
-    var inputs = $form.serializeArray();
     var formId = $form.attr('id');
     var location = $form.data('ga-location');
     var page = $form.data('ga-page');
     var title = $form.data('ga-title');
 
-    $.each(inputs, function () {
-      if (this.value) {
-        analytics.Analytics.send(
-          'event', {
-            eventCategory: 'SecurityForms',
-            eventAction: formId,
-            eventLabel: this.name,
-            location: location,
-            page: page,
-            title: title
-          }
-        );
+    function sendEvent (action, label) {
+      analytics.Analytics.send(
+        'event', {
+          eventCategory: 'SecurityForms',
+          eventAction: action,
+          eventLabel: label,
+          location: location,
+          page: page,
+          title: title
+        }
+      );
+    }
+
+    $form.on('submit', function () {
+      // send event for every used form field name
+      var inputs = $form.serializeArray();
+      $.each(inputs, function () {
+        if (this.value) {
+          sendEvent(formId, this.name);
+        }
+      });
+    });
+
+    $('.js-FormAnalytics-click', $form[0]).click(function () {
+      // send click event, e.g. for print or export
+      var $element = $(this);
+      var eventDetails = $element.data('click-track').split(',');
+      if (eventDetails.length === 2) {
+        sendEvent(eventDetails[0], eventDetails[1]);
       }
     });
   }
