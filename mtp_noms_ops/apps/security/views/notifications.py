a2a529387a928e6984cb3f31d9973207ca8c0c80
Ian Brechin <ian.brechin@gmail.com>
2019-04-29 13:55:26 +0100

    Add pagination to notification sections

--- mtp_noms_ops/apps/security/views/notifications.py
+++ mtp_noms_ops/apps/security/views/notifications.py
@@ -37,21 +37,21 @@ class NotificationListView(TemplateView):
         prison_filter = ','.join(p['nomis_id'] for p in self.request.user_prisons)
         context['monitored_credits'] = self.get_credit_list(
             session,
-            self.request.GET.get('monitored_credits_page', 1),
+            'monitored_credits_page',
             monitored=True,
             received_at__gte=start_date,
             received_at__lt=end_date
         )
         context['monitored_disbursements'] = self.get_disbursement_list(
             session,
-            self.request.GET.get('monitored_disbursements_page', 1),
+            'monitored_disbursements_page',
             monitored=True,
             created__gte=start_date,
             created__lt=end_date
         )
         context['not_whole_credits'] = self.get_event_list(
             session,
-            self.request.GET.get('not_whole_credits_page', 1),
+            'not_whole_credits_page',
             rule='NWN',
             for_credit=True,
             triggered_at__gte=start_date,
@@ -60,7 +60,7 @@ class NotificationListView(TemplateView):
         )
         context['not_whole_disbursements'] = self.get_event_list(
             session,
-            self.request.GET.get('not_whole_disbursements_page', 1),
+            'not_whole_disbursements_page',
             rule='NWN',
             for_disbursement=True,
             triggered_at__gte=start_date,
@@ -69,7 +69,7 @@ class NotificationListView(TemplateView):
         )
         context['high_amount_credits'] = self.get_event_list(
             session,
-            self.request.GET.get('high_amount_credits_page', 1),
+            'high_amount_credits_page',
             rule='HA',
             for_credit=True,
             triggered_at__gte=start_date,
@@ -78,7 +78,7 @@ class NotificationListView(TemplateView):
         )
         context['high_amount_disbursements'] = self.get_event_list(
             session,
-            self.request.GET.get('high_amount_disbursements_page', 1),
+            'high_amount_disbursements_page',
             rule='HA',
             for_disbursement=True,
             triggered_at__gte=start_date,
@@ -87,7 +87,7 @@ class NotificationListView(TemplateView):
         )
         context['frequent_senders'] = self.get_event_list(
             session,
-            self.request.GET.get('frequent_senders_page', 1),
+            'frequent_senders_page',
             rule='CSFREQ',
             group_by='sender_profile',
             triggered_at__gte=start_date,
@@ -96,7 +96,7 @@ class NotificationListView(TemplateView):
         )
         context['frequent_recipients'] = self.get_event_list(
             session,
-            self.request.GET.get('frequent_recipients_page', 1),
+            'frequent_recipients_page',
             rule='DRFREQ',
             group_by='recipient_profile',
             triggered_at__gte=start_date,
@@ -105,7 +105,7 @@ class NotificationListView(TemplateView):
         )
         context['from_many_senders'] = self.get_event_list(
             session,
-            self.request.GET.get('from_many_senders_page', 1),
+            'from_many_senders_page',
             rule='CSNUM',
             group_by='prisoner_profile',
             triggered_at__gte=start_date,
@@ -114,7 +114,7 @@ class NotificationListView(TemplateView):
         )
         context['to_many_recipients'] = self.get_event_list(
             session,
-            self.request.GET.get('to_many_recipients_page', 1),
+            'to_many_recipients_page',
             rule='DRNUM',
             group_by='prisoner_profile',
             triggered_at__gte=start_date,
@@ -123,7 +123,7 @@ class NotificationListView(TemplateView):
         )
         context['to_many_prisoners'] = self.get_event_list(
             session,
-            self.request.GET.get('to_many_prisoners_page', 1),
+            'to_many_prisoners_page',
             rule='CPNUM',
             group_by='sender_profile',
             triggered_at__gte=start_date,
@@ -132,7 +132,7 @@ class NotificationListView(TemplateView):
         )
         context['from_many_prisoners'] = self.get_event_list(
             session,
-            self.request.GET.get('from_many_prisoners_page', 1),
+            'from_many_prisoners_page',
             rule='DPNUM',
             group_by='recipient_profile',
             triggered_at__gte=start_date,
@@ -141,22 +141,23 @@ class NotificationListView(TemplateView):
         )
         return context
 
-    def get_event_list(self, session, page, **filters):
+    def get_event_list(self, session, page_param, **filters):
         event_list = self.get_object_list(
-            '/events', session, page, ordering='-triggered_at', **filters
+            '/events', session, page_param, ordering='-triggered_at', **filters
         )
         for event in event_list['results']:
             if 'group_by' in filters:
                 populate_totals(event[filters['group_by']], 'last_4_weeks')
         return event_list
 
-    def get_credit_list(self, session, page, **filters):
-        return self.get_object_list('/credits', session, page, **filters)
+    def get_credit_list(self, session, page_param, **filters):
+        return self.get_object_list('/credits', session, page_param, **filters)
 
-    def get_disbursement_list(self, session, page, **filters):
-        return self.get_object_list('/disbursements', session, page, **filters)
+    def get_disbursement_list(self, session, page_param, **filters):
+        return self.get_object_list('/disbursements', session, page_param, **filters)
 
-    def get_object_list(self, path, session, page, **filters):
+    def get_object_list(self, path, session, page_param, **filters):
+        page = int(self.request.GET.get(page_param, 1))
         offset = (page - 1) * self.page_size
         filters = {k: v for k, v in filters.items() if v}
         try:
@@ -178,5 +179,6 @@ class NotificationListView(TemplateView):
             'results': parse_date_fields(data.get('results', [])),
             'total_count': total_count,
             'page_count': page_count,
-            'page': page
+            'page': page,
+            'page_param': page_param
         }
