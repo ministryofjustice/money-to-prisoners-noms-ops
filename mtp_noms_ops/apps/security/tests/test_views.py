575a3078ba15472b979f8a6d2acee58a89c4ddd2
Ian Brechin <ian.brechin@gmail.com>
2019-05-07 12:36:18 +0100

    Fix disbursements list not showing up on prisoner profile
    This was due to a change in method ordering meaning that one thing
    was not initialised at the correct time. However, the way in which
    disbursements were retrieved on the prisoner profile page was due
    to be updated to the nested resource anyway, so I have done so.

--- mtp_noms_ops/apps/security/tests/test_views.py
+++ mtp_noms_ops/apps/security/tests/test_views.py
@@ -1203,7 +1203,9 @@ class PrisonerDetailViewTestCase(SecurityViewTestCase):
             reverse('security:prisoner_detail', kwargs={'prisoner_id': 1})
         )
         self.assertContains(response, 'Stop monitoring this prisoner')
-        self.assertEqual(responses.calls[-2].request.body, b'{"last_result_count": 4}')
+        for call in responses.calls:
+            if call.request.path_url == '/searches/1/':
+                self.assertEqual(call.request.body, b'{"last_result_count": 4}')
 
     @responses.activate
     @override_nomis_settings
@@ -1234,13 +1236,15 @@ class PrisonerDetailViewTestCase(SecurityViewTestCase):
             '?pin=1'
         )
 
-        self.assertEqual(
-            json.loads(responses.calls[-3].request.body.decode()),
-            {
-                'description': 'A1409AE JAMES HALLS',
-                'endpoint': '/prisoners/1/credits/',
-                'last_result_count': 4,
-                'site_url': '/en-gb/security/prisoners/1/',
-                'filters': [{'field': 'ordering', 'value': '-received_at'}],
-            },
-        )
+        for call in responses.calls:
+            if call.request.path_url == '/searches/':
+                self.assertEqual(
+                    json.loads(call.request.body.decode()),
+                    {
+                        'description': 'A1409AE JAMES HALLS',
+                        'endpoint': '/prisoners/1/credits/',
+                        'last_result_count': 4,
+                        'site_url': '/en-gb/security/prisoners/1/',
+                        'filters': [{'field': 'ordering', 'value': '-received_at'}],
+                    },
+                )
