2392f4c834f5a94a3e00dadb8ec3b2cd8a1e3829
Marco Fucci <info@marcofucci.com>
2019-07-30 12:40:12 +0100

    Add PrisonersFormV2

--- mtp_noms_ops/apps/security/tests/test_forms.py
+++ mtp_noms_ops/apps/security/tests/test_forms.py
@@ -15,6 +15,7 @@ from security.forms.object_list import (
     SendersForm,
     SendersFormV2,
     PrisonersForm,
+    PrisonersFormV2,
     CreditsForm,
     CreditsFormV2,
     DisbursementsForm,
@@ -336,6 +337,9 @@ class SenderFormV2TestCase(SecurityFormTestCase):
 
 
 class PrisonerFormTestCase(SecurityFormTestCase):
+    """
+    TODO: delete after search V2 goes live.
+    """
     form_class = PrisonersForm
     api_list_path = '/prisoners/'
 
@@ -403,6 +407,124 @@ class PrisonerFormTestCase(SecurityFormTestCase):
             mock_prison_response(rsps)
             form = self.form_class(self.request, data={'page': '1', 'prison': 'ABC'})
         self.assertFalse(form.is_valid())
+
+
+class PrisonerFormV2TestCase(SecurityFormTestCase):
+    """
+    Tests related to the PrisonersFormV2.
+    """
+    form_class = PrisonersFormV2
+    api_list_path = '/prisoners/'
+
+    def test_blank_form(self):
+        """
+        Test that if no data is passed in, the default values are used instead.
+        """
+        with responses.RequestsMock() as rsps:
+            mock_prison_response(rsps)
+            mock_empty_response(rsps, self.api_list_path)
+
+            form = self.form_class(self.request, data={})
+            self.assertTrue(form.is_valid())
+            self.assertListEqual(form.get_object_list(), [])
+            self.assertDictEqual(
+                parse_qs(rsps.calls[-1].request.url.split('?', 1)[1]),
+                parse_qs('offset=0&limit=20&ordering=-sender_count'),
+            )
+
+        self.assertDictEqual(
+            form.cleaned_data,
+            {
+                'page': 1,
+                'ordering': '-sender_count',
+                'prison': [],
+                'simple_search': '',
+            },
+        )
+        self.assertDictEqual(
+            form.get_query_data(),
+            {'ordering': '-sender_count'},
+        )
+        self.assertEqual(
+            form.query_string,
+            'ordering=-sender_count',
+        )
+
+    def test_valid(self):
+        """
+        Test that if data is passed in, the API query string is constructed as expected.
+        """
+        with responses.RequestsMock() as rsps:
+            prisons = mock_prison_response(rsps)
+            mock_empty_response(rsps, self.api_list_path)
+
+            form = self.form_class(
+                self.request,
+                data={
+                    'page': 2,
+                    'ordering': '-credit_total',
+                    'prison': [
+                        prisons[0]['nomis_id'],
+                    ],
+                    'simple_search': 'Joh',
+                },
+            )
+
+            self.assertTrue(form.is_valid())
+            self.assertListEqual(form.get_object_list(), [])
+            self.assertDictEqual(
+                parse_qs(rsps.calls[-1].request.url.split('?', 1)[1]),
+                parse_qs('offset=20&limit=20&ordering=-credit_total&prison=IXB&simple_search=Joh'),
+            )
+
+        self.assertDictEqual(
+            form.cleaned_data,
+            {
+                'page': 2,
+                'ordering': '-credit_total',
+                'prison': [
+                    prisons[0]['nomis_id'],
+                ],
+                'simple_search': 'Joh',
+            },
+        )
+
+        self.assertDictEqual(
+            form.get_query_data(),
+            {
+                'ordering': '-credit_total',
+                'prison': [
+                    prisons[0]['nomis_id'],
+                ],
+                'simple_search': 'Joh',
+            },
+        )
+
+    def test_invalid(self):
+        """
+        Test validation errors.
+        """
+        scenarios = [
+            ValidationScenario(
+                {'page': '0'},
+                {'page': ['Ensure this value is greater than or equal to 1.']},
+            ),
+            ValidationScenario(
+                {'ordering': 'prison'},
+                {'ordering': ['Select a valid choice. prison is not one of the available choices.']},
+            ),
+            ValidationScenario(
+                {'prison': ['invalid']},
+                {'prison': ['Select a valid choice. invalid is not one of the available choices.']},
+            ),
+        ]
+
+        for scenario in scenarios:
+            with responses.RequestsMock() as rsps:
+                mock_prison_response(rsps)
+                form = self.form_class(self.request, data=scenario.data)
+            self.assertFalse(form.is_valid())
+            self.assertDictEqual(form.errors, scenario.errors)
 
 
 class CreditFormTestCase(SecurityFormTestCase):
