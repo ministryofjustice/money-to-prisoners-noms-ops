708fb8445633c533c2f1bede16715ba594d6eec9
Marco Fucci <info@marcofucci.com>
2019-09-05 15:16:48 +0100

    Refactor GA events for form submitted
    Before:
    The event category for submitting a form was "SecurityForms"
    and one event per filter used was sent.
    
    After:
    We send one event per filter with the new event category
    "form-submitted-single-filter" and an extra one which combines
    all filters used in the form as a comma separated string
    with the event category "form-submitted-combined-filters".

--- mtp_noms_ops/assets-src/javascripts/modules/form-analytics.js
+++ mtp_noms_ops/assets-src/javascripts/modules/form-analytics.js
@@ -10,7 +10,7 @@ exports.FormAnalytics = {
   bindForm: function () {
     var $form = $(this);
     var formId = $form.attr('id');
-    var initialInputs = $form.serializeArray();
+    var initialInputs = $form.find(":input:not(:hidden)").serializeArray();
 
     function sendEvent (category, action, label) {
       analytics.Analytics.send(
@@ -18,9 +18,10 @@ exports.FormAnalytics = {
       );
     }
 
-    $form.on('submit', function () {
+    $form.on('submit', function (e) {
       // send event for every changed form field name
-      var inputs = $form.serializeArray();
+      var inputs = $form.find(":input:not(:hidden)").serializeArray();
+      var nonEmptyFilters = [];
       $.each(inputs, function () {
         var changed = true;
         for (var i = 0; i < initialInputs.length; i++) {
@@ -30,12 +31,23 @@ exports.FormAnalytics = {
           }
         }
         if (this.value) {
+          nonEmptyFilters.push(this.name);
+
           if (changed) {
+            // TODO: delete after search V2 goes live ?
             sendEvent('SecurityFormsFilter', formId, this.name);
           }
-          sendEvent('SecurityForms', formId, this.name);
+          sendEvent('SecurityForms', formId, this.name);  // TODO: delete after search V2 goes live.
+
+          // send one event per filter
+          sendEvent('form-submitted-single-filter', formId, this.name);
         }
       });
+
+      // send also an event for all combined filters used
+      if (nonEmptyFilters.length) {
+        sendEvent('form-submitted-combined-filters', formId, nonEmptyFilters.sort().join(','));
+      }
     });
 
     $('.js-FormAnalytics-click', $form[0]).click(function () {
