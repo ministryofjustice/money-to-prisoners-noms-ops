c3f873de227ddf33bdb14bfc9d02e8a4416f0f65
Marco Fucci <info@marcofucci.com>
2019-09-13 14:45:25 +0100

    Change Get help to Help and feedback
    This changes:
    - the text 'Get help' in main nav and footer to 'Help and feedback'
    - the title 'Get help' on feedback pages to 'Get help and leave
    feedback'
    - the thank you message after submitting the form
    
    This is so that people understand that the form can be used
    for leaving feedback as well.

--- mtp_noms_ops/apps/feedback/urls.py
+++ mtp_noms_ops/apps/feedback/urls.py
@@ -1,13 +1,31 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
-from mtp_common.views import GetHelpView as BaseGetHelpView, GetHelpSuccessView
+from django.utils.translation import gettext
+from mtp_common.views import (
+    GetHelpView as BaseGetHelpView,
+    GetHelpSuccessView as BaseGetHelpSuccessView,
+)
 
 
-class GetHelpView(BaseGetHelpView):
+class FeedbackContextMixin:
+    """
+    Used to override the page title using a context variable.
+    """
+
+    def get_context_data(self, **kwargs):
+        kwargs['get_help_title'] = gettext('Get help and leave feedback')
+        return super(FeedbackContextMixin, self).get_context_data(**kwargs)
+
+
+class GetHelpView(FeedbackContextMixin, BaseGetHelpView):
     success_url = reverse_lazy('feedback_success')
     ticket_subject = 'MTP NOMS Ops Feedback'
     ticket_tags = ['feedback', 'mtp', 'noms-ops', settings.ENVIRONMENT]
+
+
+class GetHelpSuccessView(FeedbackContextMixin, BaseGetHelpSuccessView):
+    template_name = 'feedback/success.html'
 
 
 urlpatterns = [
