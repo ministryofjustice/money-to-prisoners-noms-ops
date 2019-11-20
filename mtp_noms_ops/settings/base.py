f423e940b42f7e4e36be7397ca2307fd5d483e68
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-11-20 21:45:38 +0000

    Install bare-bones prometheus metrics

--- mtp_noms_ops/settings/base.py
+++ mtp_noms_ops/settings/base.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
 PROJECT_APPS = (
     'anymail',
     'mtp_common',
+    'mtp_common.metrics',
     'widget_tweaks',
     'prisoner_location_admin',
     'security',
@@ -81,6 +82,9 @@ MIDDLEWARE_CLASSES = (
 
 HEALTHCHECKS = []
 AUTODISCOVER_HEALTHCHECKS = True
+
+METRICS_USER = os.environ.get('METRICS_USER', 'prom')
+METRICS_PASS = os.environ.get('METRICS_PASS', 'prom')
 
 # security tightening
 # some overridden in prod/docker settings where SSL is ensured
