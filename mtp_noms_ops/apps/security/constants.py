391d6f3c2c5c45bc62a96a985d579287902cda98
Toby Petty <tobypetty1@gmail.com>
2021-02-08 17:22:00 +0000

    MTP-1738 Added explanatory comment against string constant

--- mtp_noms_ops/apps/security/constants.py
+++ mtp_noms_ops/apps/security/constants.py
@@ -27,5 +27,6 @@ CHECK_DETAIL_FORM_MAPPING = {
     )
 }
 
+# This is as custom-defined exception within the API service that we match against
 CHECK_AUTO_ACCEPT_UNIQUE_CONSTRAINT_ERROR = \
     'An existing AutoAcceptRule is present for this DebitCardSenderDetails/PrisonerProfile pair'
