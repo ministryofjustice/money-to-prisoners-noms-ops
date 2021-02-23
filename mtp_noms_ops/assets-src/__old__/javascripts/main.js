9d8ae1bcc832b0db5475b8b68dc7e845c815ea77
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-02-23 12:37:23 +0000

    Update print trigger component

--- mtp_noms_ops/assets-src/__old__/javascripts/main.js
+++ mtp_noms_ops/assets-src/__old__/javascripts/main.js
@@ -3,7 +3,6 @@
 
   // common
   require('upload').Upload.init();
-  require('print').Print.init();
   require('mailcheck-warning').MailcheckWarning.init(
     '.mtp-account-management input[type=email]',
     ['justice.gov.uk'],
