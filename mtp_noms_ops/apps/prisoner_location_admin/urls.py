a4ce2b8cbf8d02de3a4c9683a11ce644228f3411
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2025-02-26 12:21:00 +0000

    Appeasing flake8

--- mtp_noms_ops/apps/prisoner_location_admin/urls.py
+++ mtp_noms_ops/apps/prisoner_location_admin/urls.py
@@ -5,6 +5,9 @@ from prisoner_location_admin import required_permissions, views
 
 
 urlpatterns = [
-    re_path(r'^$', user_test(required_permissions)(views.LocationFileUploadView.as_view()),
-        name='location_file_upload'),
+    re_path(
+        r'^$',
+        user_test(required_permissions)(views.LocationFileUploadView.as_view()),
+        name='location_file_upload',
+    ),
 ]
